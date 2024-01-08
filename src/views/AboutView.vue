f526e925cf846c799a6b15ab6543dc09c2a45559
Aakeefah Jones <aakeefahj@gmail.com>
2024-01-08 21:51:36 +0200

    jsonfilesUpdated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row">
+      <h1>This is the about page</h1>
+    </div>
+    <div class="row" v-if="about">
+    <p class="lead" v-for="title in about" :key="title">
+      {{ about }}
+    </p>
+  </div>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    about(){
+      return this.$store.state.about
+    }
+  },
+  mounted(){
+    this.$store.dispatch('fetchAbout')
+  }
+};
+</script>
