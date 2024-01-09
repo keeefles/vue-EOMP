3302cbbf88968cb75e8951d730c318014eb9c8b5
Aakeefah Jones <aakeefahj@gmail.com>
2024-01-09 19:47:39 +0200

    content added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <NavBar></NavBar>
     <router-view/>
+    <Spinner/>
   <Footer></Footer>
 </template>
 
 <script>
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
+import Spinner from './components/Spinner.vue';
 
   export default {
     components: {
       NavBar,
       Footer,
-    }
+      Spinner,
+    },
+    data(){
+      return {
+        isloading: false,
+      };
+    },
   }
 </script>
 
